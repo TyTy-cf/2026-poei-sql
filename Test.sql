6edb7536281b937eaa2131f4c9ee80b2250c5c65
YsDnG <ya.denguir@gmail.com>
2026-06-10 16:16:29 +0200

    exos Done

--- Test.sql
+++ Test.sql
@@ -6,3 +6,18 @@ ALTER TABLE student
 ALTER TABLE student
     ADD COLUMN gender ENUM('H','M','A')
 
+ALTER TABLE student
+DROP
+COLUMN name;
+
+
+CREATE TABLE course
+(
+    id INT(6) AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY(id)
+    name    VARCHAR(20) NOT NULL,
+    hours   INT(1000) NOT NULL,
+    teacher VARCHAR(50) NOT NULL,
+
+
+)
