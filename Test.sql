dc04b3c33a0982c6a7010aebb2e2dca3da4e8c90
YsDnG <ya.denguir@gmail.com>
2026-06-10 14:41:40 +0200

    sql debut

--- Test.sql
+++ Test.sql
@@ -1,23 +1,3 @@
 ALTER TABLE student
     ADD COLUMN height INT(3) DEFAULT NULL
 
-
-
-ALTER TABLE student
-    ADD COLUMN gender ENUM('H','M','A')
-
-ALTER TABLE student
-DROP
-COLUMN name;
-
-
-CREATE TABLE course
-(
-    id INT(6) AUTO_INCREMENT NOT NULL,
-    PRIMARY KEY(id)
-    name    VARCHAR(20) NOT NULL,
-    hours   INT(1000) NOT NULL,
-    teacher VARCHAR(50) NOT NULL,
-
-
-)
