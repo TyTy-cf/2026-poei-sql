d7ecb58aeec78bd439414ddc5f63ae4b1457be90
Pierre Allimant <drosalys@ordi-10.cyberfull.local>
2026-06-15 14:17:20 +0200

    exosuite

--- exo_1/exo.sql
+++ exo_1/exo.sql
@@ -23,3 +23,22 @@ SELECT COUNT(*) FROM students
 WHERE sex = "F" AND height > 160;
 
 
+HAVING UNE PROCHAINE FOIS;
+
+
+SELECT * FROM `students`
+WHERE height >= 170 AND height <= 190;
+
+
+SELECT * FROM `students`
+WHERE height = 160
+   OR height = 170
+   OR height = 180
+   OR height = 190;
+
+
+INSERT INTO students (id, last_name, first_name, height, sex)
+VALUES (NULL, "Parker", "Antony", 199, "M");
+
+
+SELECT CONCAT (FLOOR(height/100), ".", height%100, "m") FROM students;
