6154de78905690e719bdc095e143eee6501904e8
Lunnay1 <doigniesvalentin@gmail.com>
2026-06-15 14:31:20 +0200

    Exercises 12 to 15

--- exo_1/exo.sql
+++ exo_1/exo.sql
@@ -11,38 +11,31 @@ WHERE (sex = "M" AND height < 160) OR (sex = "F" AND height > 160);
 
 -- 3
 
-SELECT MIN(height) AS "Taille Minimale"
+SELECT MIN(height) AS shorter
 FROM students;
 
 -- 4
 
-SELECT AVG(height) AS "Moyenne Taille Hommes"
+SELECT AVG(height) AS avg_man_height
 FROM students
 WHERE sex = "M";
 
 -- 5
 
-SELECT COUNT(sex) AS "Hommes"
+SELECT sex AS sex, COUNT(sex) AS count
 FROM students
-WHERE sex = "M";
-
-SELECT COUNT(sex) AS "Femmes"
-FROM students
-WHERE sex = "F";
+GROUP BY sex;
 
 -- 6
 
-SELECT COUNT(sex) AS "Hommes"
+SELECT sex AS sex, COUNT(sex) AS count
 FROM students
-WHERE sex = "M" AND height < 160
-
-SELECT COUNT(sex) AS "Femmes"
-FROM students
-WHERE sex = "F" AND height > 160;
+WHERE (sex = "M" AND height < 160) OR (sex = "F" AND height > 160)
+GROUP BY sex;
 
 -- 7
 
-SELECT height, COUNT(*)
+SELECT height, COUNT(*) AS total_students_by_height
 FROM students
 GROUP BY height
 HAVING COUNT(*) > 1;
@@ -68,3 +61,31 @@ VALUES ("Parker", "Antony", 199, "M");
 
 SELECT *, CONCAT(ROUND((height/100), 2), "m") AS height_real
 FROM students;
+
+-- 12
+
+UPDATE students
+SET height = 189
+WHERE last_name = "Parker";
+
+-- 13
+
+DELETE FROM students
+WHERE first_name = "Maxine";
+
+-- 14
+
+SELECT *,
+IF (
+    sex = "M",
+    "Homme",
+    "Femme"
+) AS long_sex
+FROM students;
+
+-- 15
+
+SELECT sex AS sex, COUNT(sex) AS count
+FROM students
+WHERE (sex = "M" AND height < 160) OR (sex = "F" AND height > 160)
+GROUP BY sex;
