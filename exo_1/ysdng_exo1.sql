8c18e61df103b49bb433348563b9cb4ea34dc49f
YsDnG <ya.denguir@gmail.com>
2026-06-15 13:41:37 +0200

    exos en cours

--- exo_1/ysdng_exo1.sql
+++ exo_1/ysdng_exo1.sql
@@ -1,1 +1,52 @@
-SELECT * FROM students;
+SELECT *
+FROM students;
+
+
+-- 2) ------
+SELECT *
+FROM `students`
+WHERE (
+    height < 160
+        AND
+    sex = 'M'
+    )
+   OR (height > 160
+    AND
+       sex = 'F');
+
+
+-- 3) --------
+SELECT MIN(height) AS min_height
+FROM students;
+
+
+-- 4) ------
+SELECT AVG(height) AS height_Moy
+FROM students
+WHERE (sex = 'M');
+
+
+-- 5) ----
+SELECT COUNT(*) AS men
+FROM students
+WHERE(sex = 'M');
+
+-- ------------------------------
+SELECT COUNT(*) AS men
+FROM students
+WHERE(sex = 'F');
+
+-- 6) ------------
+SELECT COUNT(*) AS men_less_160
+FROM students
+WHERE(
+    sex = 'M'
+        AND
+    height < 160
+    );
+--7) --------------
+SELECT height, COUNT(*) AS total_students
+FROM students
+GROUP BY height
+HAVING COUNT(*) > 1;
+
