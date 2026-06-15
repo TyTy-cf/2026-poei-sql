f725c888ae3a8cb17af4765d314c99232ba9addb
YsDnG <ya.denguir@gmail.com>
2026-06-15 14:19:41 +0200

    suite

--- exo_1/ysdng_exo1.sql
+++ exo_1/ysdng_exo1.sql
@@ -29,24 +29,62 @@ WHERE (sex = 'M');
 -- 5) ----
 SELECT COUNT(*) AS men
 FROM students
-WHERE(sex = 'M');
+WHERE (sex = 'M');
 
 -- ------------------------------
 SELECT COUNT(*) AS men
 FROM students
-WHERE(sex = 'F');
+WHERE (sex = 'F');
 
 -- 6) ------------
 SELECT COUNT(*) AS men_less_160
 FROM students
-WHERE(
-    sex = 'M'
-        AND
-    height < 160
-    );
+WHERE (
+          sex = 'M'
+              AND
+          height < 160
+          );
 --7) --------------
 SELECT height, COUNT(*) AS total_students
 FROM students
 GROUP BY height
 HAVING COUNT(*) > 1;
+
+--8) ------------
+SELECT *
+FROM students
+WHERE (
+          height > 160
+              AND
+          height < 180)
+
+
+--9) ----------------
+SELECT *
+FROM students
+WHERE height IN
+      (160, 170, 180, 190);
+
+-- 10) -------------
+INSERT INTO students (last_name, first_name, height, sex)
+VALUES ('Parker', 'Anthony', 199, 'M');
+
+
+---11) ---------
+SELECT *,
+       CONCAT(FORMAT(height/100,2),'m')
+FROM students;
+-- 12) -----------
+UPDATE students
+SET height = 189
+WHERE last_name = 'Parker';
+
+-- 13) ---------------
+DELETE
+FROM students
+WHERE first_name = 'maxine';
+
+--
+
+
 
