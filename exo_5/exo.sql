b486b20729682beb6ef4096fcc1c4a3ed1b6945d
Lunnay1 <doigniesvalentin@gmail.com>
2026-06-17 14:36:36 +0200

    Exercice 5 amorcé

---
+++ exo_5/exo.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE db_spotifish;
+
+CREATE TABLE account (
+    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    birth_date date,
+    created_at datetime NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    gender ENUM('H', 'F', 'NB', 'A', 'NR') NOT NULL,
+    name varchar(255) NOT NULL,
+    newsletter tinyint(1) NOT NULL,
+    password varchar(255) NOT NULL
+);
+
+CREATE TABLE subscription (
+    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    cost double NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE table account_subscription (
+    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    account_id int(11) NOT NULL REFERENCES account(id),
+    subscription_id int(11) NOT NULL REFERENCES subscription(id),
+    effective_at datetime NOT NULL,
+    finished_at datetime,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE artist (
+    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    url_banner varchar(255)
+    PRIMARY KEY(id)
+)
