1321a32e0744bf9f69132510ca95596bd79764dc
Lunnay1 <doigniesvalentin@gmail.com>
2026-06-17 15:35:41 +0200

    Module 5 terminé

--- exo_5/exo.sql
+++ exo_5/exo.sql
@@ -1,56 +1,80 @@
 CREATE DATABASE db_spotifish;
 
 CREATE TABLE account (
-    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    id int(8) NOT NULL AUTO_INCREMENT,
+    name varchar(80) NOT NULL,
+    email varchar(120) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
     birth_date date,
-    created_at datetime NOT NULL,
-    email varchar(255) UNIQUE NOT NULL,
     gender ENUM('H', 'F', 'NB', 'A', 'NR') NOT NULL,
-    name varchar(255) NOT NULL,
     newsletter tinyint(1) NOT NULL,
-    password varchar(255) NOT NULL
+    created_at datetime NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE subscription (
-    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    id int(8) UNIQUE NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     cost double NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE table account_subscription (
-    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
-    account_id int(11) NOT NULL REFERENCES account(id),
-    subscription_id int(11) NOT NULL REFERENCES subscription(id),
+    id int(8) UNIQUE NOT NULL AUTO_INCREMENT,
+    account_id int(8) NOT NULL REFERENCES account(id),
+    subscription_id int(8) NOT NULL REFERENCES subscription(id),
     effective_at datetime NOT NULL,
     finished_at datetime,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE artist (
-    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
+    id int(8) UNIQUE NOT NULL AUTO_INCREMENT,
+    name varchar(80) NOT NULL,
+    email varchar(120) UNIQUE NOT NULL,
     password varchar(255) NOT NULL,
     url_banner varchar(255) NOT NULL,
-    certificate longtext NOT NULL,
+    certificate text NOT NULL,
     created_at datetime NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE album (
-    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
-    artist_id int(11) NOT NULL REFERENCES artist(id),
+    id int(8) UNIQUE NOT NULL AUTO_INCREMENT,
+    artist_id int(8) NOT NULL REFERENCES artist(id),
     name varchar(255) NOT NULL,
-    released_at datetime NOT NULL,
-    finished_at datetime NOT NULL,
+    published_at datetime NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE song (
-    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    id int(8) UNIQUE NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    duration int(11) NOT NULL,
-    is_explicit tinyint(1) NOT NULL,
+    duration int(6) NOT NULL,
+    explicit tinyint(1) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE album_song (
+    album_id int(8) NOT NULL REFERENCES album(id),
+    song_id int(8) NOT NULL REFERENCES song(id),
+    PRIMARY KEY(album_id, song_id)
+);
+
+CREATE TABLE playlist (
+    id int(8) UNIQUE NOT NULL AUTO_INCREMENT,
+    account_id int(8) NOT NULL REFERENCES account(id),
+    name varchar(80) NOT NULL,
+    created_at datetime NOT NULL,
+    updated_at datetime,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE playlist_song (
+    id int(8) UNIQUE NOT NULL AUTO_INCREMENT,
+    playlist_id int(8) NOT NULL REFERENCES playlist(id),
+    song_id int(8) NOT NULL REFERENCES song(id),
+    position int(10) NOT NULL,
+    add_at datetime NOT NULL,
     PRIMARY KEY(id)
 );
