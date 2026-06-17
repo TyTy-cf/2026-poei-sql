e77186ecc1d6ec82546296e9e8c20233f5604534
charfi1816 <charfi.nesrine2015@gmail.com>
2026-06-17 12:49:41 +0200

    modification

--- exo_4/exo_4.sql
+++ exo_4/exo_4.sql
@@ -46,9 +46,6 @@ FROM account a
 JOIN library l ON a.id = l.account_id
 ORDER BY a.name;
 
-
-
-
 ### 6/ Afficher la valeur (somme du prix des jeux) de la bibliothèque (library) d'un compte (account)
 
 
@@ -61,25 +58,92 @@ GROUP BY a.name;
 
 ### 7/ Afficher les nicknames utilisés plusieurs fois
 
+SELECT a.nicknames 
+FROM account a
+GROUP BY a.nicknames
+HAVING COUNT(*) > 1;
+
+
 ### 8/ Afficher par jeux, le nombre de fois où il a été acheté
 
+SELECT g.name, COUNT(l.account_id)
+FROM game g
+JOIN library l ON  g.id = l.game_id
+GROUP BY g.name
+
+
 ### 9/ Afficher par jeux, son revenu total à son éditeur
+SELECT g.name, SUM(g.price)
+FROM game g
+JOIN library l ON g.id = l.game_id
+GROUP BY g.name
+
 
 ### 10/ Afficher par genre, son nombre de fois où il a été vendu
 
+SELECT gr.name , COUNT(l.account_id)
+FROM genre gr
+join game_genre gg on gr.id = gg.genre_id
+join library l on gg.game_id = l.game_id
+GROUP by gr.name;
+
+
 ### 11/ Afficher le top 3 des jeux les plus vendu
+
+
+select g.name , COUNT(l.account_id) AS total_ventes
+FROM game g
+join library l ON g.id = l.game_id
+GROUP by g.name
+ORDER BY total_ventes DESC
+limit 3;
+
 
 ### 12/ Afficher le top 3 des jeux les plus joués (temps de jeu cumulé parmi toutes les library les plus élevé)
 
+SELECT g.name
+FROM game g
+JOIN library l ON g.id = l.game_id
+ORDER BY l.game_time DESC
+LIMIT 3;
+
 ### 13/ Afficher les différents jeux par année, sous une même colonne
 
+SELECT CONCAT(g.name,'/',YEAR(g.published_at)) 
+FROM  game g
+GROUP BY YEAR(g.published_at)
+ORDER by YEAR(g.published_at) DESC;
+
 ### 14/ Le jeu le plus ancien
+ SELECT g.*
+ FROM game g
+ ORDER BY published_at  ASC
+ limit 1;
+
 
 ### 15/ Afficher les jeux avec leur note moyenne (table comment, colonne rank)
 
+SELECT g.name , AVG(C.RA)
+FROM game g
+JOIN  comment c on g.id = c.game_id
+GROUP BY g.name ;
+
+
 ### 16/ Afficher le jeu ayant le plus de commentaire négatif (colonne down_votes)
 
+select g.name , SUM(c.down_votes) as best_comment
+FROM game g 
+join comment c on g.id = c.game_id
+GROUP BY g.name
+ORDER by best_comment
+LIMIT 1 ;
+
+
+
 ### 17/ Afficher les jeux dont la moyenne des commentaires (rank) est supérieur à la moyenne globale
+
+
+
 
 ### 18/ Afficher les account n’ayant jamais acheté de jeu
 
