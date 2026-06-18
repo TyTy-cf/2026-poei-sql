b54d2ec348ac404e8934ae34071996418e60923b
Stéphane Leite <sleite1910@gmail>
2026-06-18 10:01:01 +0200

    exo 14

--- exo6.sql
+++ exo6.sql
@@ -15,10 +15,10 @@ ORDER BY a.published_at DESC LIMIT 9;
 ---2
 
 SELECT art.name AS "Nom artiste",
-       a.name   AS "Nom album", YEAR (a.published_at) AS "Année de sortie"
+       a.name   AS "Nom album",
+       YEAR(a.published_at) AS "Année de sortie"
 FROM artist art
-    LEFT JOIN album a
-ON a.artist_id = art.id
+    LEFT JOIN album a ON a.artist_id = art.id
 ORDER BY art.name, a.published_at;
 
 ---3
@@ -46,10 +46,11 @@ GROUP BY al.id, al.name;
 
 ---5
 
-SELECT art.name AS "Nom artiste", YEAR (art.created_at) AS "Année"
+SELECT art.name AS "Nom artiste",
+       YEAR(art.created_at) AS "Année"
 FROM artist art
 WHERE YEAR (art.created_at) = (
-    SELECT MIN (YEAR (created_at))
+    SELECT MIN(YEAR(created_at))
     FROM artist
     );
 
@@ -95,3 +96,65 @@ WHERE a.id NOT IN (SELECT account_id
                    FROM playlist);
 
 ---11
+
+SELECT p.name AS "Nom playlist",
+       a.name AS "Nom propriétaire",
+       COUNT(*) AS "Nombre de likes"
+FROM playlist p
+
+LEFT JOIN account_like_playlist alp ON alp.playlist_id = p.id
+LEFT JOIN account a ON alp.account_id = a.id
+
+GROUP BY p.id;
+
+
+---12
+
+SELECT a.id,
+       a.name,
+       COUNT(p.id) AS "NB de playlist"
+
+FROM account a
+
+         LEFT JOIN playlist p ON p.account_id = a.id
+
+GROUP BY a.id;
+
+---13
+
+SELECT p.name AS "Playlist",
+       SEC_TO_TIME(SUM(s.duration)) AS "Durée playlist"
+
+FROM playlist p
+
+    LEFT JOIN playlist_song ps ON p.id = ps.playlist_id
+    LEFT JOIN song s ON s.id = ps.song_id
+
+GROUP BY p.id;
+
+---14
+
+SELECT a.name,
+    YEAR(CURDATE()) - YEAR(birth_date) AS "âge"
+
+FROM account a;
+
+---15
+
+
+
+
+
+
+
+---17
+
+SELECT
+    s.name,
+    ROUND((COUNT(a_s.id) * s.cost) / 100, 2) AS cout_revient
+
+FROM `subscription` s
+
+         LEFT JOIN account_subscription a_s ON a_s.subscription_id = s.id
+
+GROUP BY s.id;
